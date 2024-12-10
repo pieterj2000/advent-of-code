02f4baf25fdcbb6a431d88ecab08fa3816e8f6cb
pieterj2000 <19341833+pieterj2000@users.noreply.github.com>
2024-12-10 23:26:02 +0100

    scaffold

---
+++ doe.sh
@@ -0,0 +1,36 @@
+if [ -z "$1" ]
+  then
+    echo "geen jaar en nummer"
+    exit
+fi
+if [ -z "$2" ]
+  then
+    echo "geen nummer"
+    exit
+fi
+
+cd "$(dirname "$0")"
+echo $1
+mkdir -p $1/$2
+cd $1/$2
+touch $2a.ex.input
+touch $2a.input
+cat > $2a.hs <<- EOM
+
+inputFile = "7a.input"
+main = do
+    input <- readFile inputFile
+    print $ calc . parse $ input
+
+parse :: String -> [[Int]]
+parse = map (map read . words) . lines
+
+calc :: [[Int]] -> Int
+calc reports = length $ filter (isSafe . increments) reports
+    where
+        increments levels = zipWith (-) (tail levels) levels
+        isSafe incrs = (all (\i -> i < 0 && i > -4) incrs) || (all (\i -> i > 0 && i < 4) incrs )
+
+EOM
+
+touch $2b.hs
